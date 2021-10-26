217f8caca2d163fd943469d1f07d7db7d39f29d1
LoicLaroulette <larouletteloic05@gmail.com>
2021-10-26 15:26:28 +0400

    miniproject

---
+++ script.js
@@ -0,0 +1,68 @@
+function playTheGame() {
+    let answer = confirm("Do you want to play?");
+    console.log(answer);
+if(answer == false){
+    alert("No problem, Goodbye")
+    return
+}
+else {
+    let randomNumber = Math.floor(Math.random()*10);
+    let chances =0;
+    while(chances<3){
+        let userNumber=prompt("Enter a number between 0 and 10")
+        userNumber=Number(userNumber)
+        let good= chooseNum ( userNumber, randomNumber);
+            if(good ){
+                let game = test(userNumber,randomNumber)
+                if(game ){
+                    chances = 3
+                    return
+                }
+                
+                console.log(userNumber,randomNumber)
+                chances++// = chances+ 1
+            }
+    }
+}
+}
+playTheGame()
+function chooseNum(numNum, randomNumber){
+ if(numNum>=0 && numNum <=10){
+        return true;
+    }
+else if(numNum<0 && numNum >10){
+        alert("Sorry it’s not a good number, Goodbye")
+        return false;
+    }
+else{
+        alert("Sorry Not a number, Goodbye")
+        return false;
+    }
+}
+function test(userNumber,computerNumber){
+
+if(userNumber == computerNumber){
+    alert ("Winner");
+    return true
+}
+ else if(userNumber> computerNumber){
+    prompt("Your number is bigger then the computer’s, guess again")
+    return false
+}
+ else if(userNumber<computerNumber){
+    prompt("Your number is smaller then the computer’s, guess again")
+    return false
+}
+else{
+    console.log("Loser")
+    return false
+}
+        
+
+// if(chances >= 3){
+// 	alert("no more chances")
+// 	return
+// }
+
+}
+
